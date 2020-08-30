ffe4d45c9ce9b1c2f51d5579c1274e6732693b69
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2020-08-30 17:32:21 +0200

    Web project generated in Spring boot

---
+++ hotel/src/test/java/pl/polsl/hotelapplication/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.hotelapplication.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
