227b980dc10b50754b6a088db1f79382565cdcd2
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2020-09-19 09:28:36 +0200

    deleted old projects

--- HotelApp/Hotel/src/main/java/pl/polsl/Hotel/dao/HotelRepo.java
+++
@@ -1,9 +0,0 @@
-package pl.polsl.Hotel.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.polsl.Hotel.dao.entity.Hotel;
-
-@Repository
-public interface HotelRepo extends CrudRepository<Hotel,Long> {
-}
