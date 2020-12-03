dd67f4be30bfe996016b69450eeab4019aaefa6b
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2020-12-03 21:26:43 +0100

    added new repository

---
+++ HotelApplication/frontend/hotel-app-client/src/app/_authentication/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AuthenticationService } from '../_services/authentication.service';
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    const currentUser = this.authenticationService.currentUserValue;
+
+    if (this.authenticationService.isUserLoggedIn) {
+
+      if (state.url === '/greeting' && currentUser.roleCode === 'CLI') {
+        this.router.navigate(['/unauthorized'], {skipLocationChange: true});
+        return false;
+      }
+
+      return true;
+    }
+
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+
+
+}
