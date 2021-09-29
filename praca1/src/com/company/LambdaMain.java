2f4d03d500580ce022f34e48b58eb5c4e121587c
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2021-09-29 20:41:18 +0200

    added functional interfaces

--- praca1/src/com/company/LambdaMain.java
+++ praca1/src/com/company/LambdaMain.java
@@ -1,9 +1,10 @@
 package com.company;
 
+import java.util.concurrent.Callable;
 import java.util.function.*;
 
 public class LambdaMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Consumer<String> consumer = new Consumer<String>() {
             @Override
             public void accept(String s) {
@@ -25,6 +26,20 @@ public class LambdaMain {
 
         BiFunction<String,String,Integer> biFunction = (str1, str2) -> str1.length() + str2.length();
         System.out.println(biFunction.apply("kuba", "ola"));
+
+        Runnable runnable = () -> System.out.println("hej!");
+        runnable.run();
+
+        Callable<Double> callable = () -> Math.random();
+        System.out.println(callable.call());
+
+        BiConsumer<String,String> biConsumer;
+
+        BiPredicate<String,String> biPredicate;
+
+        UnaryOperator<String> unaryOperator;
+
+        BinaryOperator<String> binaryOperator;
     }
 
 }
