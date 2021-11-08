738d0872361771d18070c25c25fb889cbd4a0238
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2021-11-08 20:24:57 +0100

    added methods in optional

--- java/praca1/src/com/company/OptionalMain.java
+++ java/praca1/src/com/company/OptionalMain.java
@@ -8,5 +8,14 @@ public class OptionalMain {
         MyOptional<String> myOptional = MyOptional.of("a");
         myOptional.ifPresent(System.out::println);
         MyOptional.empty();
+        System.out.println(myOptional.get());
+        //myOptional.filter()
+        //myOptional.map();
+        //myOptional.ifPresentOrElse();
+        System.out.println(myOptional.isEmpty());
+        System.out.println(myOptional.isPresent());
+//        myOptional.orElse();
+//        myOptional.orElseGet();
+//        myOptional.orElseThrow();
     }
 }
