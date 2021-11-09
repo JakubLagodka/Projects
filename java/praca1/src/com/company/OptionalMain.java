6ce3f3f1ac091757afbd93bdebb4d3f75d391cfd
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2021-11-09 10:02:42 +0100

    added methods call in optional

--- java/praca1/src/com/company/OptionalMain.java
+++ java/praca1/src/com/company/OptionalMain.java
@@ -1,21 +1,34 @@
 package com.company;
 
+import java.io.IOException;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class OptionalMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //wywołać of i sout, przetestowac metody
         MyOptional<String> myOptional = MyOptional.of("a");
         myOptional.ifPresent(System.out::println);
         MyOptional.empty();
         System.out.println(myOptional.get());
-        //myOptional.filter()
-        //myOptional.map();
-        //myOptional.ifPresentOrElse();
+
+        Predicate<String> predicate = String::isEmpty;
+        System.out.println(myOptional.filter(predicate));
+
+        Function<String, Integer> function = String::length;
+        System.out.println(myOptional.map(function));
+        myOptional.ifPresentOrElse(System.out::println, () -> System.out.println("hej"));
         System.out.println(myOptional.isEmpty());
         System.out.println(myOptional.isPresent());
-//        myOptional.orElse();
-//        myOptional.orElseGet();
-//        myOptional.orElseThrow();
+        System.out.println(myOptional.orElse("null value"));
+        System.out.println(myOptional.orElseGet(String::new));
+        System.out.println(myOptional.orElseThrow(Exception::new));
+
+        MyOptional<String> emptyOptional = MyOptional.empty();
+        emptyOptional.ifPresentOrElse(System.out::println, () -> System.out.println("hej"));
+        System.out.println(emptyOptional.orElse("null value"));
+        System.out.println(emptyOptional.orElseGet(String::new));
+        System.out.println(emptyOptional.orElseThrow(Exception::new));
     }
 }
