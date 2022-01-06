fdaa4a70857f4808318b3557d472f374761c3155
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-01-06 21:02:00 +0100

    added UserServiceImplSpec

--- java/shop/shop/src/test/groovy/pl/lagodka/shop/mapper/UserMapperImplSpec.groovy
+++ java/shop/shop/src/test/groovy/pl/lagodka/shop/mapper/UserMapperImplSpec.groovy
@@ -1,5 +1,6 @@
 package pl.lagodka.shop.mapper
 
+import org.springframework.data.history.RevisionMetadata
 import pl.lagodka.shop.model.dao.Role
 import pl.lagodka.shop.model.dao.User
 import pl.lagodka.shop.model.dto.UserDto
@@ -7,14 +8,14 @@ import spock.lang.Specification
 
 import java.time.LocalDateTime
 
-class UserMapperImplSpec extends Specification{
+class UserMapperImplSpec extends Specification {
     def userMapper = new UserMapperImpl()
 
-    def "should map User to UserDto"(){
+    def 'should map User to UserDto'() {
         given:
-        def user = new User(1,"Jakub","Lagodka","kuba","password","mail@gmail.com",
-                LocalDateTime.of(2022,1,6,13,30,50),"kuba",
-                LocalDateTime.of(2022,1,5,12,40,50),"jan",[new Role(id: 1, name: "admin")])
+        def user = new User(1, "Jakub", "Lagodka", "kuba", "password", "mail@gmail.com",
+                LocalDateTime.of(2022, 1, 6, 13, 30, 50), "kuba",
+                LocalDateTime.of(2022, 1, 5, 12, 40, 50), "jan", [new Role(id: 1, name: "admin")])
 
         when:
         def result = userMapper.toDto(user)
@@ -33,5 +34,28 @@ class UserMapperImplSpec extends Specification{
         result.password == null
         result.revisionNumber == null
         result.revisionType == null
+    }
+
+    def 'should map UserDto to User'() {
+        given:
+        def userDto = new UserDto(1, "Jakub", "Lagodka", "kuba", "password", "password", "mail@gmail.com",
+                LocalDateTime.of(2022, 1, 6, 13, 30, 50), "kuba",
+                LocalDateTime.of(2022, 1, 5, 12, 40, 50), "jan",
+                RevisionMetadata.RevisionType.DELETE, 0)
+        when:
+        def result = userMapper.toDao(userDto)
+
+        then:
+        result.id == userDto.id
+        result.createdBy == userDto.createdBy
+        result.createdDate == userDto.createdDate
+        result.firstName == userDto.firstName
+        result.lastModifiedBy == userDto.lastModifiedBy
+        result.lastModifiedDate == userDto.lastModifiedDate
+        result.lastName == userDto.lastName
+        result.login == userDto.login
+        result.mail == userDto.mail
+        result.password == userDto.password
+        result.roles == null
     }
 }
---
+++ java/shop/shop/src/test/groovy/pl/lagodka/shop/service/impl/UserServiceImplSpec.groovy
@@ -0,0 +1,70 @@
+package pl.lagodka.shop.service.impl
+
+import org.springframework.data.domain.Pageable
+import org.springframework.security.crypto.password.PasswordEncoder
+import pl.lagodka.shop.model.dao.Role
+import pl.lagodka.shop.model.dao.User
+import pl.lagodka.shop.repository.RoleRepository
+import pl.lagodka.shop.repository.UserRepository
+import spock.lang.Specification
+
+class UserServiceImplSpec extends Specification {
+    def userRepository = Mock(UserRepository)
+    def passwordEncoder = Mock(PasswordEncoder)
+    def roleRepository = Mock(RoleRepository)
+    def userService = new UserServiceImpl(userRepository, passwordEncoder, roleRepository)
+
+    def 'should return user by id'() {
+        given:
+        def id = 1
+
+        when:
+        userService.getById(id)
+
+        then:
+        1 * userRepository.getById(id)
+        0 * _
+    }
+
+    def 'should delete user'() {
+        given:
+        def id = 1
+
+        when:
+        userService.delete(id)
+
+        then:
+        1 * userRepository.deleteById(id)
+        0 * _
+    }
+
+    def 'should return page of users'() {
+        given:
+        def pageable = Mock(Pageable)
+
+        when:
+        userService.getPage(pageable)
+
+        then:
+        1 * userRepository.findAll(pageable)
+        0 * _
+    }
+
+    def 'should save user'() {
+        given:
+        def user = Mock(User)
+        def role = new Role(id: 1, name: "user")
+
+        when:
+        userService.create(user)
+
+        then:
+        1 * user.getPassword() >> "password"
+        1 * passwordEncoder.encode("password") >> "haslo"
+        1 * user.setPassword("haslo")
+        1 * roleRepository.findByName("ROLE_USER") >> Optional.of(role)
+        1 * user.setRoles(Collections.singletonList(role))
+        1 * userRepository.save(user)
+        0 * _
+    }
+}
