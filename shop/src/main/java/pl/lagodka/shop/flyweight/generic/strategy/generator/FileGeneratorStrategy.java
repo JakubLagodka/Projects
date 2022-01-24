0cce5b9871cd63a24104c541a6d5fa91561e2e3c
JakubLagodka <67454685+JakubLagodka@users.noreply.github.com>
2022-01-24 18:18:51 +0100

    Delete shop/src directory

--- shop/src/main/java/pl/lagodka/shop/flyweight/generic/strategy/generator/FileGeneratorStrategy.java
+++
@@ -1,8 +0,0 @@
-package pl.lagodka.shop.flyweight.generic.strategy.generator;
-
-import pl.lagodka.shop.flyweight.generic.strategy.GenericStrategy;
-import pl.lagodka.shop.flyweight.model.FileType;
-
-public interface FileGeneratorStrategy extends GenericStrategy<FileType> {
-    byte[] generateFile();
-}
--- shop/src/main/java/pl/lagodka/shop/flyweight/model/FileType.java
+++
@@ -1,5 +0,0 @@
-package pl.lagodka.shop.flyweight.model;
-
-public enum FileType {
-    PDF, CSV, JSON, XLS
-}
