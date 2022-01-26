6f618dbdcb5969acb52fce54ddbf49369869d82d
JakubLagodka <67454685+JakubLagodka@users.noreply.github.com>
2022-01-26 14:08:00 +0100

    removing old files

--- HotelApplication/backend/hotel/src/main/java/pl/lagodka/hotel/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.lagodka.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.lagodka.hotel.model.dao.Room;
-
-public interface RoomRepository extends JpaRepository<Room,Long> {
-}
--- HotelApplication/backend/hotel/src/test/groovy/pl/lagodka/hotel/mapper/ReservationMapperImplSpec.groovy
+++
@@ -1,6 +0,0 @@
-package pl.lagodka.hotel.mapper
-
-import spock.lang.Specification
-
-class ReservationMapperImplSpec extends Specification{
-}
