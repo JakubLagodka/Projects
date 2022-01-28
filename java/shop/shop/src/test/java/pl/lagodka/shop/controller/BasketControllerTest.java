8408cd836c9b5733862c853fe01619980cc74422
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-01-28 20:27:55 +0100

    fixed BasketControllerTest

--- java/shop/shop/src/test/java/pl/lagodka/shop/controller/BasketControllerTest.java
+++ java/shop/shop/src/test/java/pl/lagodka/shop/controller/BasketControllerTest.java
@@ -10,13 +10,15 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.lagodka.shop.model.dao.Basket;
 import pl.lagodka.shop.model.dao.Product;
+import pl.lagodka.shop.model.dao.User;
 import pl.lagodka.shop.model.dto.BasketDto;
-import pl.lagodka.shop.model.dto.UserDto;
+import pl.lagodka.shop.repository.BasketRepository;
 import pl.lagodka.shop.repository.ProductRepository;
+import pl.lagodka.shop.repository.UserRepository;
 
 import javax.transaction.Transactional;
-
 import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -38,19 +40,17 @@ public class BasketControllerTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private BasketRepository basketRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     @WithMockUser(username = "john")
     void shouldGetBasket() throws Exception {
 
-        mockMvc.perform(get("/api/basket")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @WithMockUser(username = "john")
-    void shouldAddProductToBasket() throws Exception {
-        Product save = productRepository.save(Product.builder()
+        Product product = productRepository.save(Product.builder()
                 .name("pen")
                 .price(9.99)
                 .available(true)
@@ -62,11 +62,80 @@ public class BasketControllerTest {
                 .lastModifiedDate(LocalDateTime.of(2022, 1, 15, 12, 40, 50))
                 .build());
 
+        User user = userRepository.save(User.builder()
+                .firstName("John")
+                .lastName("John")
+                .login("john")
+                .mail("john@gmail.com")
+                .password("pass")
+                .build());
+                basketRepository.save(Basket.builder()
+                .product(product)
+                .quantity(10)
+                .user(user)
+                .build());
+//        basketRepository.save(Basket.builder()
+//                .product(Product.builder()
+//                        .name("pen")
+//                        .price(9.99)
+//                        .available(true)
+//                        .quantity(10)
+//                        .createdBy("user")
+//                        .createdDate(LocalDateTime.of(2022, 1, 5, 12, 40, 50))
+//                        .imageUrl("http:://https://m")
+//                        .lastModifiedBy("jan")
+//                        .lastModifiedDate(LocalDateTime.of(2022, 1, 15, 12, 40, 50))
+//                        .build())
+//                .quantity(10)
+//                .user(User.builder()
+//                        .firstName("John")
+//                        .lastName("John")
+//                        .login("john")
+//                        .mail("john@gmail.com")
+//                        .password("pass")
+//                        .build())
+//                .build());
+
+
+        mockMvc.perform(get("/api/basket")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "john")
+    void shouldAddProductToBasket() throws Exception {
+
+        Product product = productRepository.save(Product.builder()
+                .name("pen")
+                .price(9.99)
+                .available(true)
+                .quantity(10)
+                .createdBy("user")
+                .createdDate(LocalDateTime.of(2022, 1, 5, 12, 40, 50))
+                .imageUrl("http:://https://m")
+                .lastModifiedBy("jan")
+                .lastModifiedDate(LocalDateTime.of(2022, 1, 15, 12, 40, 50))
+                .build());
+
+        User user = userRepository.save(User.builder()
+                .firstName("John")
+                .lastName("John")
+                .login("john")
+                .mail("john@gmail.com")
+                .password("pass")
+                .build());
+        basketRepository.save(Basket.builder()
+                .product(product)
+                .quantity(10)
+                .user(user)
+                .build());
+
         mockMvc.perform(post("/api/basket")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content(objectMapper.writeValueAsString(BasketDto.builder()
-                                .productId(1L)
-                                .quantity(10)
+                                .productId(product.getId())
+                                .quantity(0)
                                 .build())))
                 .andExpect(status().isOk());
     }
@@ -74,6 +143,32 @@ public class BasketControllerTest {
     @Test
     @WithMockUser(username = "john")
     void shouldClearBasket() throws Exception {
+
+        Product product = productRepository.save(Product.builder()
+                .name("pen")
+                .price(9.99)
+                .available(true)
+                .quantity(10)
+                .createdBy("user")
+                .createdDate(LocalDateTime.of(2022, 1, 5, 12, 40, 50))
+                .imageUrl("http:://https://m")
+                .lastModifiedBy("jan")
+                .lastModifiedDate(LocalDateTime.of(2022, 1, 15, 12, 40, 50))
+                .build());
+
+        User user = userRepository.save(User.builder()
+                .firstName("John")
+                .lastName("John")
+                .login("john")
+                .mail("john@gmail.com")
+                .password("pass")
+                .build());
+        basketRepository.save(Basket.builder()
+                .product(product)
+                .quantity(10)
+                .user(user)
+                .build());
+
         mockMvc.perform(delete("/api/basket")
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
@@ -82,6 +177,32 @@ public class BasketControllerTest {
     @Test
     @WithMockUser(username = "john")
     void shouldDeleteProductByProductId() throws Exception {
+
+        Product product = productRepository.save(Product.builder()
+                .name("pen")
+                .price(9.99)
+                .available(true)
+                .quantity(10)
+                .createdBy("user")
+                .createdDate(LocalDateTime.of(2022, 1, 5, 12, 40, 50))
+                .imageUrl("http:://https://m")
+                .lastModifiedBy("jan")
+                .lastModifiedDate(LocalDateTime.of(2022, 1, 15, 12, 40, 50))
+                .build());
+
+        User user = userRepository.save(User.builder()
+                .firstName("John")
+                .lastName("John")
+                .login("john")
+                .mail("john@gmail.com")
+                .password("pass")
+                .build());
+        basketRepository.save(Basket.builder()
+                .product(product)
+                .quantity(10)
+                .user(user)
+                .build());
+
         Product save = productRepository.save(Product.builder()
                 .name("pen")
                 .price(9.99)
