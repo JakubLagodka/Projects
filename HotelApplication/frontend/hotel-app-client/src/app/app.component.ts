8405c41c5b67b8986467330c9a04256e31ff856b
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-09-27 17:42:50 +0200

    deleted unnecessary space below calendarSubmit()

--- HotelApplication/frontend/hotel-app-client/src/app/app.component.ts
+++ HotelApplication/frontend/hotel-app-client/src/app/app.component.ts
@@ -86,7 +86,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
       return;
     }
-
     // this.calendarService = new CalendarService(new RoomService(this.http), new HotelNightService(this.http, this.authenticationService));
     if (this.authenticationService.isUserLoggedIn)
     {
