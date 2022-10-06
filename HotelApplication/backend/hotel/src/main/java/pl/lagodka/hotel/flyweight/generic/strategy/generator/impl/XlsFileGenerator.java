9b4dfc19f7cc70a55fede10a447cf112a1cb95fe
Jakub Lagodka <jakub.lagodka.pl@gmail.com>
2022-10-06 16:00:39 +0200

    making order with projects more

--- HotelApplication/backend/hotel/src/main/java/pl/lagodka/hotel/flyweight/generic/strategy/generator/impl/XlsFileGenerator.java
+++ HotelApplication/backend/hotel/src/main/java/pl/lagodka/hotel/flyweight/generic/strategy/generator/impl/XlsFileGenerator.java
@@ -21,7 +21,6 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class XlsFileGenerator implements FileGeneratorStrategy {
-
 private final ReservationRepository reservationRepository;
 
     @Override
